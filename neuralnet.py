a11e877ba5091639bbdfe9838f0835ef92c40d8b
Thomas Garcia <thomasjgarcia1@gmail.com>
2015-07-14 22:44:15 -0700

    Added biases and training

--- neuralnet.py
+++ neuralnet.py
@@ -20,7 +20,7 @@ class NeuralNet(object):
         for i in range(len(layers) - 1):
             # TODO Find correct range for initial values
             self.weights.append(
-                np.random.uniform(-1, 1, [layers[i], layers[i + 1]]))
+                np.random.uniform(-1, 1, [layers[i] + 1, layers[i + 1]]))
 
     def __call__(self, data):
         """
@@ -32,19 +32,36 @@ class NeuralNet(object):
         and each column is an output feature.
         """
         for weight in self.weights:
+            bias = np.ones((len(data), 1))
             # Sigmoid is the default activation function for neural nets
-            data = sigmoid(np.dot(data, weight))
+            data = sigmoid(np.dot(np.hstack((bias, data)), weight))
         return data
 
-    def check_gradient(self, input, expected_output):
+    def train(self, input, expected_output):
+        """
+        Train the neural net using the given input and output.
+        """
         array, shapes = NeuralNet.unroll(self.weights)
-        fun = lambda x: NeuralNet.cost(NeuralNet.roll(x, shapes), 
+        def fun(x):
+            cost, grad = NeuralNet.cost(NeuralNet.roll(x, shapes),
+                                        input, expected_output)
+            return cost, NeuralNet.unroll(grad)[0]
+        result = minimize(fun, array, jac=True)
+        self.weights = NeuralNet.roll(result.x, shapes)
+
+    def check_gradient(self, input, expected_output):
+        """
+        Check whether cost properly calculates gradients.
+        Result should be close to zero.
+        """
+        array, shapes = NeuralNet.unroll(self.weights)
+        fun = lambda x: NeuralNet.cost(NeuralNet.roll(x, shapes),
                                        input,
                                        expected_output)[0]
         grad = lambda x: NeuralNet.unroll(
-                NeuralNet.cost(
-                    NeuralNet.roll(x, shapes), input, expected_output)[1])[0]
-        x = NeuralNet.roll(grad(array), shapes)[0]
+            NeuralNet.cost(NeuralNet.roll(x, shapes),
+                           input,
+                           expected_output)[1])[0]
         return check_grad(fun, grad, array)
 
     @staticmethod
@@ -66,7 +83,8 @@ class NeuralNet(object):
         for weight in weights:
             # Sigmoid is the default activation function for neural nets
             inputs.append(input)
-            result = sigmoid(input.dot(weight))
+            bias = np.ones((len(input), 1))
+            result = sigmoid(np.hstack((bias, input)).dot(weight))
             results.append(result)
             input = result
         # This cost function is convex, unlike squared error,
@@ -78,30 +96,35 @@ class NeuralNet(object):
         deltas = [delta]
         for i in reversed(range(0, len(weights) - 1)):
             # g(x) (1- g(x)) is the derivative of sigmoid
-            delta = (delta.dot(weights[i+1].transpose()) *
+            delta = (delta.dot(weights[i+1][1:,].transpose()) *
                      results[i] * (1 - results[i]))
             deltas.insert(0, delta)
-        for delta in deltas:
-            print(delta.shape)
         gradients = []
         for i in range(0, len(deltas)):
-            gradients.append(inputs[i].transpose().dot(deltas[i]) / num_samples)
-        for grad in gradients:
-            print(grad.shape)
-        print("")
+            bias = np.ones((len(inputs[i]), 1))
+            gradients.append(
+                np.hstack((bias, inputs[i])).transpose().dot(deltas[i]) /
+                num_samples)
         return cost, gradients
 
     @staticmethod
     def unroll(weights):
-        w = weights[0].ravel()
+        """
+        Unroll a list of weights into a single ndarray. Return the array,
+        along with a list of shapes for the original weights.
+        """
+        array = weights[0].ravel()
         shapes = [weights[0].shape]
         for weight in weights[1:]:
             shapes.append(weight.shape)
-            w = np.concatenate((w, weight.ravel()))
-        return w, shapes
+            array = np.concatenate((array, weight.ravel()))
+        return array, shapes
 
     @staticmethod
     def roll(array, shapes):
+        """
+        Splits one ndarray into multiple, based on the shapes provided.
+        """
         splits = [0]
         for shape in shapes:
             splits.append(shape[0] * shape[1] + splits[len(splits)-1])
