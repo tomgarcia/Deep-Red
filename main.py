2bea311d8f6790a8bac259652541640d9bef0545
Thomas Garcia <thomasjgarcia1@gmail.com>
2015-08-01 08:15:46 -0700

    Switched from objects to closures for event handling. Not sure if it is better.

--- main.py
+++ main.py
@@ -8,83 +8,110 @@ from card import deal, tuple_from_s, s_from_tuple
 from bot import Bot
 
 
-class Gui(Thread):
-    """A PyGObject GUI for Deep Red"""
+def init(builder):
+    """Setup GUI"""
+    init_setup_window(builder)
+    init_sample_pane(builder)
+    init_play_pane(builder)
+    new_card_entry = builder.get_object("new_card_entry")
 
-    def __init__(self):
-        """Setup GUI"""
-        Thread.__init__(self)
-        self.bot = None
-        builder = Gtk.Builder.new_from_file("gui.glade")
-        self.setup_window = builder.get_object("setup_window")
-        self.window = builder.get_object("window")
-        self.play_dialog = builder.get_object("play_dialog")
-        self.pass_dialog = builder.get_object("pass_dialog")
-        self.card = None
-        self.prev_card = None
-        self.init_setup_window(builder)
-        self.init_sample_pane(builder)
-        self.init_play_pane(builder)
-        builder.get_object("add_card").connect_object(
-            "clicked", self.add_card, builder.get_object("new_card_entry"))
-        self.window.connect("delete-event", self.quit)
+    def add_card(entry):
+        """Event handler for added card."""
+        new_card = tuple_from_s(new_card_entry.get_text())
+        new_card_entry.set_text("")
+        bot.add_card(new_card)
 
-    def init_play_pane(self, builder):
-        actionbox = builder.get_object("actionbox1")
-        prev_card = builder.get_object("prevcard_entry")
-        builder.get_object("play_card").connect_object(
-            "clicked",
-            self.play_card,
-            prev_card,
-            actionbox,
-            builder.get_object("instructions"))
-        builder.get_object("button1").connect(
-            "clicked",
-            self.close)
-        builder.get_object("valid_button").connect_object(
-            "clicked",
-            self.valid,
-            actionbox)
-        builder.get_object("invalid_button").connect("clicked", self.invalid)
+    builder.get_object("add_card").connect("clicked", add_card)
 
-    def init_setup_window(self, builder):
-        builder.get_object("setup_window").connect("delete-event", self.quit)
-        builder.get_object("begin").connect_object(
-            "clicked",
-            self.begin,
-            builder.get_object("action_list"),
-            builder.get_object("actionbox"),
-            builder.get_object("actionbox1"))
+def init_play_pane(builder):
+    actionbox = builder.get_object("actionbox1")
+    prev_card_entry = builder.get_object("prevcard_entry")
+    play_dialog = builder.get_object("play_dialog")
+    pass_dialog = builder.get_object("pass_dialog")
+    label = builder.get_object("instructions")
+    card = None
+    prev_card = None
 
-    def init_sample_pane(self, builder):
-        actionbox = builder.get_object("actionbox")
-        prev_card = builder.get_object("sample_prevcard_entry")
-        card = builder.get_object("sample_card_entry")
-        valid_button = builder.get_object("valid_checkbox")
-        actionbox.set_sensitive(False)
-        valid_button.connect(
-            "toggled",
-            self.toggle_valid,
-            actionbox)
-        builder.get_object("add_sample").connect_object(
-            "clicked",
-            self.add_sample,
-            prev_card,
-            card,
-            valid_button,
-            actionbox)
+    def play_card(button):
+        """Event handler for play card button."""
+        nonlocal card, prev_card
+        prev_card = tuple_from_s(prev_card_entry.get_text())
+        prev_card_entry.set_text("")
+        play = bot.play(prev_card)
+        if play:
+            dialog = play_dialog
+            action_list = actionbox.get_children()
+            card, actions = play
+            for i in range(len(actions)):
+                action_list[i].set_active(actions[i])
+            label.set_text("Deep Red played " + s_from_tuple(card) +
+                           """. Is this valid? If the card is valid but the
+                           actions are not, simply change the actions and then
+                           click valid.""")
+        else:
+            dialog = pass_dialog
+        dialog.show_all()
 
+    def valid(button):
+        """Event handler for valid button"""
+        actions = [int(b.get_active()) for b in actionbox.get_children()]
+        bot.add_sample(card, prev_card, True, actions)
+        dialog = actionbox.get_toplevel()
+        dialog.hide()
 
-    def run(self):
-        """Start window and Gtk main loop"""
-        self.setup_window.show_all()
-        Gtk.main()
+    def invalid(button):
+        """Event handler for invalid button"""
+        bot.add_sample(card, prev_card, False)
+        bot.add_card(card)
+        dialog = button.get_toplevel()
+        dialog.hide()
 
-    def quit(self, widget, event):
-        """Close GUI"""
-        Gtk.main_quit()
+    def close(button):
+        """Default handler for dialog buttons."""
+        button.get_toplevel().hide()
 
-    def add_sample(self, prev_card_entry, card_entry, valid_button, actionbox):
+    builder.get_object("play_card").connect("clicked", play_card)
+    builder.get_object("button1").connect("clicked", close)
+    builder.get_object("valid_button").connect("clicked", valid)
+    builder.get_object("invalid_button").connect("clicked", invalid)
+
+def init_setup_window(builder):
+    action_list = builder.get_object("action_list")
+    sample_actionbox = builder.get_object("actionbox")
+    play_actionbox = builder.get_object("actionbox1")
+    setup_window = builder.get_object("setup_window")
+    window = builder.get_object("window")
+    window.connect("delete-event", quit)
+
+    def begin(button):
+        global bot
+        actions = [s.strip() for s in action_list.get_text().split(",")]
+        for action in actions:
+            sample_actionbox.add(Gtk.CheckButton(action))
+            play_actionbox.add(Gtk.CheckButton(action))
+        bot = Bot(deal(5), len(actions))
+        setup_window.hide()
+        window.show_all()
+
+    setup_window.connect("delete-event", quit)
+    builder.get_object("begin").connect("clicked", begin)
+    setup_window.show_all()
+
+def init_sample_pane(builder):
+    actionbox = builder.get_object("actionbox")
+    prev_card_entry = builder.get_object("sample_prevcard_entry")
+    card_entry = builder.get_object("sample_card_entry")
+    valid_button = builder.get_object("valid_checkbox")
+    actionbox.set_sensitive(False)
+
+    def toggle_valid(button):
+        """
+        Activate or deactivate the action checkboxes, depending on whether
+        valid checkbox is clicked.
+        """
+        actionbox.set_sensitive(button.get_active())
+
+    def add_sample(button):
         """
         Add a sample to the bot, based on the sample input fields.
         """
@@ -96,73 +123,21 @@ class Gui(Thread):
         for button in actionbox.get_children():
             actions.append(int(button.get_active()))
             button.set_active(False)
-        self.bot.add_sample(card,
+        bot.add_sample(card,
                             prev_card,
                             valid_button.get_active(),
                             actions)
         valid_button.set_active(False)
 
-    def toggle_valid(self, button, actions):
-        """
-        Activate or deactivate the action checkboxes, depending on whether
-        valid checkbox is clicked.
-        """
-        actions.set_sensitive(button.get_active())
+    valid_button.connect("toggled", toggle_valid)
+    builder.get_object("add_sample").connect("clicked", add_sample)
 
-    def add_card(self, entry):
-        """Event handler for added card."""
-        new_card = tuple_from_s(entry.get_text())
-        entry.set_text("")
-        self.bot.add_card(new_card)
-
-    def play_card(self, prev_card_entry, actionbox, label):
-        """Event handler for play card button."""
-        prev_card = tuple_from_s(prev_card_entry.get_text())
-        prev_card_entry.set_text("")
-        play = self.bot.play(prev_card)
-        if play:
-            dialog = self.play_dialog
-            actionbox = actionbox.get_children()
-            card, actions = play
-            self.card = card
-            self.prev_card = prev_card
-            for i in range(len(actions)):
-                actionbox[i].set_active(actions[i])
-            label.set_text("Deep Red played " + s_from_tuple(card) +
-                           """. Is this valid? If the card is valid but the
-                           actions are not, simply change the actions and then
-                           click valid.""")
-        else:
-            dialog = self.pass_dialog
-        dialog.show_all()
-
-    def valid(self, actionbox):
-        """Event handler for valid button"""
-        actions = [int(b.get_active()) for b in actionbox.get_children()]
-        self.bot.add_sample(self.card, self.prev_card, True, actions)
-        dialog = actionbox.get_toplevel()
-        dialog.hide()
-
-    def invalid(self, button):
-        """Event handler for invalid button"""
-        self.bot.add_sample(self.card, self.prev_card, False)
-        self.bot.add_card(self.card)
-        dialog = button.get_toplevel()
-        dialog.hide()
-
-    def close(self, button):
-        """Default handler for dialog buttons."""
-        button.get_toplevel().hide()
-
-    def begin(self, action_list, sample_actionbox, play_actionbox):
-        actions = [s.strip() for s in action_list.get_text().split(",")]
-        for action in actions:
-            sample_actionbox.add(Gtk.CheckButton(action))
-            play_actionbox.add(Gtk.CheckButton(action))
-        self.bot = Bot(deal(5), len(actions))
-        self.setup_window.hide()
-        self.window.show_all()
+def quit(widget, event):
+    """Close GUI"""
+    Gtk.main_quit()
 
 if __name__ == "__main__":
-    gui = Gui()
-    gui.start()
+    bot = None
+    builder = Gtk.Builder.new_from_file("gui.glade")
+    init(builder)
+    Gtk.main()
