91cfb14ec2e3a9eee5a1469a86d207f4bac77990
Thomas Garcia <thomasjgarcia1@gmail.com>
2016-01-30 21:17:26 -0800

    Added decision tree class and testing code. ID3 algorithm still needs some bugfixing

---
+++ decisiontree.py
@@ -0,0 +1,59 @@
+import sys
+import numpy as np
+from scipy import stats
+
+class DecisionTree:
+
+    def __init__(self, data, schema):
+        self.root = DecisionTree.make_node(data, schema, list(range(1, len(schema))))
+
+    @staticmethod
+    def make_node(data, schema, indices):
+        cls = data[0][0]
+        if np.all(data[:,0] == cls):
+            return cls
+        if len(indices) == 0:
+            return stats.mode(data[:,0])[0][0]
+        branches = []
+        index = 0
+        min_entropy = sys.maxsize
+        for i in indices:
+            avg_entropy = 0
+            for value in schema[1:]:
+                subset = data[data[:, i] == value]
+                if len(subset) == 0:
+                    continue
+                subset = subset[:,0]
+                entropy = 0
+                for cls in range(schema[0]):
+                    p = np.sum(subset == cls) / len(subset)
+                    if p == 0:
+                        continue
+                    info = -1 * np.log(p)
+                    entropy += p * info
+                # Don't divide by len(data) because it's the same for all indices
+                avg_entropy += entropy * len(subset)
+            if avg_entropy < min_entropy:
+                index = i
+                min_entropy = avg_entropy
+        indices.remove(index)
+        for value in range(schema[index]):
+            subset = data[data[:,index] == value]
+            if subset.size == 0:
+                branches.append(stats.mode(data[:,0])[0][0])
+            else:
+                branches.append(DecisionTree.make_node(subset, schema, indices))
+        indices.append(index) # indices is pass by reference, so we need to restore it
+        return DecisionTree.Node(branches, index-1)
+
+    def __call__(self, sample):
+        ptr = self.root
+        while isinstance(ptr, DecisionTree.Node):
+            ptr = ptr.branches[sample[ptr.index]]
+        return ptr
+
+    class Node:
+
+        def __init__(self, branches, index):
+            self.branches = branches
+            self.index = index
---
+++ treetest.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+import numpy as np
+from decisiontree import DecisionTree
+from card import deal
+
+def get_test_data(num_training, num_validation, num_test, fun):
+    samples = {"training": [], "validation": [], "test": []}
+    num_samples = {"training": num_training,
+                   "validation": num_validation,
+                   "test": num_test}
+    for key, count in num_samples.items():
+        for i in range(count):
+            samples[key].append(fun(*deal(2)))
+    for key in samples.keys():
+        samples[key] = np.array(samples[key])
+    return samples
+
+
+def make_play_sample(card, prev_card):
+    rank = prev_card[0] == card[0]
+    suite = prev_card[1] == card[1]
+    if prev_card[0] == card[0]:
+        is_correct = 1
+    elif prev_card[1] == card[1]:
+        if prev_card[0] == 3 and card[0] < 3:
+            is_correct = 0
+        elif prev_card[0] == 5 and card[0] != 5:
+            is_correct = 0
+        else:
+            is_correct = 1
+    else:
+        is_correct = 0
+    return [is_correct, *card, *prev_card, rank, suite]
+
+samples = get_test_data(10, 1000, 1000, make_play_sample)
+d = DecisionTree(samples["training"], [2, 13, 4, 13, 4, 2, 2])
+for key in samples.keys():
+    print(key)
+    correct = 0
+    for sample in samples[key]:
+        if d(sample[1:]) == sample[0]:
+            correct += 1
+    print((correct / len(samples[key])) * 100)
